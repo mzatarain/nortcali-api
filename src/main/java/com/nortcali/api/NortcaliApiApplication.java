f2f795ab04e6e3d977e8a8cf2f025a8534542ed4
Manuel Rodriguez <m.zatarain@hotmail.com>
2026-01-01 21:35:49 -0800

    first commit

---
+++ src/main/java/com/nortcali/api/NortcaliApiApplication.java
@@ -0,0 +1,13 @@
+package com.nortcali.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NortcaliApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NortcaliApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nortcali/api/NortcaliApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nortcali.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NortcaliApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
